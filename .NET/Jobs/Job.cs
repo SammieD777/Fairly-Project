3f4222c7bbb3d9bf30d3ef76976a53431afc062e
Samantha Esperanza Davila <116118170+SammieD777@users.noreply.github.com>
2023-11-12 17:33:57 -0800

    Job.cs

---
+++ .NET/Jobs/Job.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class Job
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string Requirements { get; set; }
+
+        public LookUp JobTypes { get; set; }
+
+        public LookUp JobStatus { get; set; }
+
+        public LookUp Organization { get; set; }
+
+        public string Location { get; set; }
+
+        public LookUp RemoteStatus { get; set; }
+
+        public string ContactName { get; set; }
+
+        public string ContactPhone { get; set; }
+
+        public string ContactEmail { get; set; }
+
+        public DateTime EstimatedStartDate { get; set; }
+
+        public DateTime EstimatedFinishDate { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public int ModifiedBy { get; set; }
+
+
+    }
+}
